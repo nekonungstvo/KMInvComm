f14f509a07a17bb1e6ee148c03cb486e193b851e
volpe262 <volpe_262@mail.ru>
2018-03-07 18:58:08 +0100

    Fix language and /lore

--- src/main/java/Core.java
+++ src/main/java/Core.java
@@ -1,10 +1,15 @@
 package KMInvComm;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -32,7 +37,7 @@ implements Listener {
         String playerName = commandSender.getName();
         if (command.getName().equals("nrp")) {
             if (args.length == 0 || args[0].equals("help")) {
-                commandSender.sendMessage("§e----------- §fHelp: nrp §e--------------------§f\nThis command is used to get non-roleplay (NRP, OOC) items, such as pickaxe or sings.\n§e/nrp tools §f- get pickaxe and axe.\n§e/nrp signs §f- get signs and item frames.\n§e/nrp microtools §f- get tools to operate microblocks.\n§e/nrp fire §f- get flint and steel.\n");
+                commandSender.sendMessage("§e----------- §fHelp: nrp §e--------------------§f\nЭта команда используется для получения НРП-предметов, например кирки или табличек.\n§e/nrp tools §f- кирка и топор.\n§e/nrp signs §f- рамки и таблички.\n§e/nrp microtools §f- инструменты для работы с микроблоками.\n§e/nrp fire §f- огниво.\n");
                 return true;
             }
             if (args[0].equals("tools")) {
@@ -105,13 +110,27 @@ implements Listener {
             }
         } else if (command.getName().equals("lore")) {
             if (args.length == 0 || args[0].equals("help")) {
-                commandSender.sendMessage("§e----------- §fHelp: lore §e--------------------§f\nThis command allows player to add lore to the item he is currently holding. Note that GameMaster's confirmation is necessary, until than item is called \"UNCONFIRMED ITEM\"\n§e/lore add §f- add multiple lore lines\n§e/lore set §f- set lore for item.\n");
+                commandSender.sendMessage("§e----------- §fHelp: lore §e--------------------§f\nЭта команда позволяет игроку изменить описание предмета, который он держит в руке. Обратите внимание, что до подтверждения ГМом предмет не является действительным.\n§e/lore add §f- добавить строки к описанию.\n§e/lore set §f- установить описание.\n");
                 return true;
             }
             if (args[0].equals("add") || args[0].equals("set")) {
                 String sumArgs = "";
                 for (int i = 1; i < args.length; i++) {
                     sumArgs += " " + args[i];
+                }
+                String action = "";
+                if (args[0].equals("add"))
+                    action = "\nHe added lore:\n";
+                else if (args[0].equals("set"))
+                    action = "\nHe set lore:\n";
+                ItemStack itemStack = ((Player) commandSender).getItemInHand();
+                ItemMeta itemMeta = itemStack.getItemMeta();
+                try (FileWriter writer = new FileWriter("logs/lore/lore_current.log", true)) {
+                    Date date = new Date();
+                    String what = itemStack.toString();
+                    writer.write("[" + date.toString() + "] " + commandSender.getName() + " edited following item:\n" + what + action + sumArgs + '\n');
+                } catch (IOException ex) {
+                     System.out.println(ex.getMessage());
                 }
                 sumArgs = sumArgs.trim();
                 String commandText = args[0] + "lore " + sumArgs;
@@ -120,8 +139,10 @@ implements Listener {
                         scheduler.scheduleSyncDelayedTask(this, new Runnable() {
                             @Override
                             public void run() {
+                                Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "pex user " + commandSender.getName() + " add sr.*");
                                 Bukkit.getServer().dispatchCommand(commandSender, "rename &4НЕ ПОДТВЕРЖДЕННЫЙ ПРЕДМЕТ");
                                 Bukkit.getServer().dispatchCommand(commandSender, commandText);
+                                Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "pex user " + commandSender.getName() + " remove sr.*");
                             }
                         }, 1L) == -1) {
                     System.out.println("_Что-то пошло не так, команда не была выполнена!_");
