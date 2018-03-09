b905fc133a24d611325253bff2ae309b794e006a
volpe262 <volpe_262@mail.ru>
2018-03-09 18:32:45 +0100

    Add small fixes

--- src/main/java/Core.java
+++ src/main/java/Core.java
@@ -2,12 +2,16 @@ package KMInvComm;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatTabCompleteEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -42,89 +46,124 @@ implements Listener {
             }
             if (args[0].equals("tools")) {
                 String[] commandText = new String[2];
-                commandText[0] = "give "+playerName+" diamond_pickaxe 1 0 {display: {Name:\"§fАлмазная кирка\", Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5}]}";
-                commandText[1] = "give "+playerName+" diamond_axe 1 0 {display: {Name:\"§fАлмазный топор\", Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5}]}";
+                commandText[0] = "give " + playerName + " diamond_pickaxe 1 0 {display: {Name:\"§fАлмазная кирка\", Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5},{id:33, lvl:1}]}";
+                commandText[1] = "give " + playerName + " diamond_axe 1 0 {display: {Name:\"§fАлмазный топор\", Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5}, {id:33, lvl:1}]}";
                 BukkitScheduler scheduler = getServer().getScheduler();
-                    if (
-                            scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-                                @Override
-                                public void run() {
-                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[0]);
-                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[1]);
-                                }
-                            }, 1L) == -1) {
-                        System.out.println("_Что-то пошло не так, команда не была выполнена!_");
-                         return false;
-                         }
-            return true;
-            }if (args[0].equals("signs")) {
-                String[] commandText = new String[2];
-                commandText[0] = "give "+playerName+" sign 16 0";
-                commandText[1] = "give "+playerName+" item_frame 64 0";
-                BukkitScheduler scheduler = getServer().getScheduler();
-                    if (
-                            scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-                                @Override
-                                public void run() {
-                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[0]);
-                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[1]);
-                                }
-                            }, 1L) == -1) {
-                        System.out.println("_Что-то пошло не так, команда не была выполнена!_");
-                         return false;
-                         }
-            return true;
-            }if (args[0].equals("microtools")) {
-                String[] commandText = new String[3];
-                commandText[0] = "give "+playerName+" yegamolchattels:iron_chisel 1 0 {display: {Name:\"§fСтамеска для обработки\",Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5}] }";
-                commandText[1] = "give "+playerName+" yegamolchattels:club_hammer 1 0 {display: {Name:\"§fКувалда\",Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5}]}";
-                commandText[2] = "give "+playerName+" yegamolchattels:iron_chisel_point 1 0 {display: {Name: \"§fКузнечная стамеска\", Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5}] }";
-                BukkitScheduler scheduler = getServer().getScheduler();
-                    if (
-                            scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-                                @Override
-                                public void run() {
-                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[0]);
-                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[1]);
-                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[2]);
-                                }
-                            }, 1L) == -1) {
-                        System.out.println("_Что-то пошло не так, команда не была выполнена!_");
-                         return false;
-                         }
-            return true;
-            }if (args[0].equals("fire")) {
-                String commandText = "give "+playerName+" flint_and_steel 1 0 {display: {Name:\"§fОгниво\", Lore:[\"§4НРП\"]} }";
-                BukkitScheduler scheduler = getServer().getScheduler();
-                    if (
-                            scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-                                @Override
-                                public void run() {
-                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText);
-                                }
-                            }, 1L) == -1) {
-                        System.out.println("_Что-то пошло не так, команда не была выполнена!_");
-                         return false;
-                         }
-            return true;
+                if (
+                        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                            @Override
+                            public void run() {
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[0]);
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[1]);
+                            }
+                        }, 1L) == -1) {
+                    System.out.println("_Что-то пошло не так, команда не была выполнена!_");
+                    return false;
+                }
+                return true;
             }
+            if (args[0].equals("signs")) {
+                String[] commandText = new String[2];
+                commandText[0] = "give " + playerName + " sign 16 0";
+                commandText[1] = "give " + playerName + " item_frame 64 0";
+                BukkitScheduler scheduler = getServer().getScheduler();
+                if (
+                        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                            @Override
+                            public void run() {
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[0]);
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[1]);
+                            }
+                        }, 1L) == -1) {
+                    System.out.println("_Что-то пошло не так, команда не была выполнена!_");
+                    return false;
+                }
+                return true;
+            }
+            if (args[0].equals("microtools")) {
+                String[] commandText = new String[3];
+                commandText[0] = "give " + playerName + " yegamolchattels:iron_chisel 1 0 {display: {Name:\"§fСтамеска для обработки\",Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5}] }";
+                commandText[1] = "give " + playerName + " yegamolchattels:club_hammer 1 0 {display: {Name:\"§fКувалда\",Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5}]}";
+                commandText[2] = "give " + playerName + " yegamolchattels:iron_chisel_point 1 0 {display: {Name: \"§fКузнечная стамеска\", Lore:[\"§4НРП\"]}, ench: [{id:34, lvl:5}] }";
+                BukkitScheduler scheduler = getServer().getScheduler();
+                if (
+                        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                            @Override
+                            public void run() {
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[0]);
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[1]);
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText[2]);
+                            }
+                        }, 1L) == -1) {
+                    System.out.println("_Что-то пошло не так, команда не была выполнена!_");
+                    return false;
+                }
+                return true;
+            }
+            if (args[0].equals("fire")) {
+                String commandText = "give " + playerName + " flint_and_steel 1 0 {display: {Name:\"§fОгниво\", Lore:[\"§4НРП\"]} }";
+                BukkitScheduler scheduler = getServer().getScheduler();
+                if (
+                        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                            @Override
+                            public void run() {
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), commandText);
+                            }
+                        }, 1L) == -1) {
+                    System.out.println("_Что-то пошло не так, команда не была выполнена!_");
+                    return false;
+                }
+                return true;
+            }
+        } else if (command.getName().equals("setplayername")) {
+            if (!commandSender.hasPermission("KMChat.gm")) {
+                commandSender.sendMessage("§4Недостаточно прав!");
+                return false;
+            }
+            ((Player) commandSender).setDisplayName(args[0]);
+            return true;
         } else if (command.getName().equals("lore")) {
             if (args.length == 0 || args[0].equals("help")) {
                 commandSender.sendMessage("§e----------- §fHelp: lore §e--------------------§f\nЭта команда позволяет игроку изменить описание предмета, который он держит в руке. Обратите внимание, что до подтверждения ГМом предмет не является действительным.\n§e/lore add §f- добавить строки к описанию.\n§e/lore set §f- установить описание.\n");
                 return true;
             }
             if (args[0].equals("add") || args[0].equals("set")) {
-                String sumArgs = "";
-                for (int i = 1; i < args.length; i++) {
-                    sumArgs += " " + args[i];
-                }
-                String action = "";
-                if (args[0].equals("add"))
-                    action = "\nHe added lore:\n";
-                else if (args[0].equals("set"))
-                    action = "\nHe set lore:\n";
                 ItemStack itemStack = ((Player) commandSender).getItemInHand();
                 ItemMeta itemMeta = itemStack.getItemMeta();
+                itemMeta.setDisplayName("§4НЕ ПОДТВЕРЖДЕННЫЙ ПРЕДМЕТ");
+                String sumArgs = "";
+                for (int i = 1; i < args.length; i++) {
+                    sumArgs += args[i].replace('&', '§');
+                }
+                List<String> newLore = new ArrayList<String>();
+                while (true) {
+                    if (sumArgs.length() > 39) {
+                        newLore.add(sumArgs.substring(0, 39));
+                        sumArgs = sumArgs.substring(39);
+                    } else {
+                        newLore.add(sumArgs);
+                        break;
+                    }
+                }
+
+                String action = "";
+                if (args[0].equals("add")) {
+                    action = "\nHe added lore:\n";
+                    if (itemMeta.hasLore()) {
+                        List<String> helpVar = itemMeta.getLore();
+                        helpVar.addAll(newLore);
+                        itemMeta.setLore(helpVar);
+                    }
+                    else
+                        itemMeta.setLore(newLore);
+                }
+                else if (args[0].equals("set")) {
+                    itemMeta.setLore(newLore);
+                    action = "\nHe set lore:\n";
+                }
+                itemStack.setItemMeta(itemMeta);
+
+
                 try (FileWriter writer = new FileWriter("logs/lore/lore_current.log", true)) {
                     Date date = new Date();
                     String what = itemStack.toString();
@@ -132,9 +171,9 @@ implements Listener {
                 } catch (IOException ex) {
                      System.out.println(ex.getMessage());
                 }
-                sumArgs = sumArgs.trim();
+               /* sumArgs = sumArgs.trim();
                 String commandText = args[0] + "lore " + sumArgs;
-                BukkitScheduler scheduler = getServer().getScheduler();
+                 BukkitScheduler scheduler = getServer().getScheduler();
                 if (
                         scheduler.scheduleSyncDelayedTask(this, new Runnable() {
                             @Override
@@ -147,7 +186,7 @@ implements Listener {
                         }, 1L) == -1) {
                     System.out.println("_Что-то пошло не так, команда не была выполнена!_");
                     return false;
-                }
+                }*/
             }
             return true;
         }
